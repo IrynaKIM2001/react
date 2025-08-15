bdc9237fe3752b9094e40b5912c8d7ffba2a3ab2
Iryna <ira2001tu@gmail.com>
2025-08-15 11:37:01 +0300

    Lesson-3, HW Task-1, Part-2

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {routes} from "./Router/Routes.tsx";
-import {RouterProvider} from "react-router";
+import {BrowserRouter} from "react-router/internal/react-server-client";
+import App from "./App.tsx";
+import {Route, Routes} from "react-router";
+import {UsersComponent} from "./Components/UsersComponent.tsx";
+import {PostsComponent} from "./Components/PostsComponent.tsx";
+import {CommentsComponent} from "./Components/CommentsComponent.tsx";
+import {ProductsComponent} from "./Components/ProductsComponent.tsx";
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>
+    .render(<BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<App/>}>
+                        <Route path={'users'} element={<UsersComponent/>}/>
+                        <Route path={'posts'} element={<PostsComponent/>}/>
+                        <Route path={'comments'} element={<CommentsComponent/>}/>
+                        <Route path={'products'} element={<ProductsComponent/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
 )
